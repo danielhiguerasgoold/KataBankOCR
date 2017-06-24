4d8982dfb96fdfe1b3e187a2be17e087114fdc02
Daniel Higueras Goold <d.h.goold@gmail.com>
2017-06-24 22:06:32 +0200

    Removed unnecesary print

--- src/main/java/com/github/danielhiguerasgoold/katabankocr/Main.java
+++ src/main/java/com/github/danielhiguerasgoold/katabankocr/Main.java
@@ -16,7 +16,6 @@ public class Main {
                 new FiveOCRDigit(), new SixOCRDigit(), new SevenOCRDigit(), new EightOCRDigit(), new NineOCRDigit(), new ZeroOCRDigit());
         OCRDigitFile ocrDigitFile = new OCRDigitFile(fileLines.asList());
         List<String> ocrStringDigits = ocrDigitFile.getOCRDigitStringChunks();
-        System.out.println(new OneOCRDigit().matches("     |  |"));
         for(String s : ocrStringDigits) {
             ocrDigits.stream().filter(ocrDigit -> ocrDigit.matches(s)).forEach(ocrDigit -> {
                 System.out.print(ocrDigit.getValue());
